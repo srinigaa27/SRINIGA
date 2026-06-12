40e120ad1d992aedb583b0424a41266c3c9e3e35
srinigaa27 <sriniga319@gmail.com>
2026-06-12 11:00:04 +0530

    Added operator

---
+++ operator.js
@@ -0,0 +1,10 @@
+//Arithmetic
+var a=10
+var b=20
+console.log(a+b)//addition
+console.log(a-b)//subtraction
+console.log(a*b)//multiplication
+console.log(a/b)//division
+console.log(a%b)//modulus
+console.log(a**b)//exponentiation
+
