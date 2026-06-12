b33c2efff253caa14dda70c462e4dfae14811bfc
srinigaa27 <sriniga319@gmail.com>
2026-06-12 11:20:27 +0530

    added assignment

--- operator.js
+++ operator.js
@@ -1,4 +1,5 @@
 //Arithmetic
+console.log("Arithmetic operator")
 var a=10
 var b=20
 console.log(a+b)//addition
@@ -7,4 +8,13 @@ console.log(a*b)//multiplication
 console.log(a/b)//division
 console.log(a%b)//modulus
 console.log(a**b)//exponentiation
+
+//Assignment
+console.log("Assignment operator")
+var c=10
+console.log(c+=30)
+console.log(c-=30)
+console.log(c*=30)
+console.log(c/=30)
+
 
