9ae84389bde466e10d6ded10687202a20dd19afb
srinigaa27 <sriniga319@gmail.com>
2026-06-12 14:56:30 +0530

    added datatypes

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+//data types
+console.log("primitive data types")
+
+//numbers
+console.log("DT Numbers")
+var age=45
+console.log(age)
+
+//string
+console.log("DT string")
+var name="sree"
+console.log(name)
+
+//boolean
+console.log("DT boolean")
+var isstudent=false
+console.log(isstudent)
+
+//null
+console.log("DT null")
+var emptyelement=null
+console.log(null)
+
+//BigInt
+console.log("BigInt")
+var Big=1234567890098765432112345677890
+console.log(Big)
+
+//undefined
+console.log("undefined")
+var undefined
+console.log(undefined)
+
+//Symbol
+console.log("Symbol")
+var Symbolvalue=Symbol('Symbol')
+console.log(Symbol)
