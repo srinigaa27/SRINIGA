d058e0e29da001a93ca134165d4a927f72cef9be
srinigaa27 <sriniga319@gmail.com>
2026-06-12 10:30:34 +0530

    variable example

---
+++ variable.js
@@ -0,0 +1,14 @@
+//variable declaration using var
+var a=20
+console.log(a)
+
+//variable declaration using let
+{
+let name="sriniga"
+console.log(name) 
+}
+
+
+//variable declaration using const
+const name="vetias"
+console.log(name)
