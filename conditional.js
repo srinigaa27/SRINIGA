721b26078112ca0e1381fc6b697940e951de5f26
srinigaa27 <sriniga319@gmail.com>
2026-06-13 09:41:40 +0530

    added exercise

---
+++ conditional.js
@@ -0,0 +1,12 @@
+var light="white"
+
+if(light=="green"){
+    console.log("Go!")
+}
+else if(light=="yellow"){
+    console.log("wait")
+}
+else{
+    console.log("invalid light colour")
+}
+console.log("outside if")
